4d58eb760bb8fa6934d9a67cb7c047d2ec8d44e2
Iasmyn Cristyne 2InfoB <iasmyncastro009@gmail.com>
2025-08-15 07:24:18 -0300

    Update Atividade3.cpp

--- Atividade3.cpp
+++ Atividade3.cpp
@@ -4,10 +4,10 @@
 
 int main(){
 
-    int n1 = 0;
+    int num1 = 0;
     printf("Informe um numero: ");
-    scanf("%d", &n1);
-    printf(" %d \n %d %d \n %d %d %d \n %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d %d %d \n ", n1, n1 + 1, n1 + 2, n1 + 3, n1 + 4, n1 + 5, n1 +6, n1 + 7, n1 + 8, n1 + 9 , n1 + 10, n1 + 11, n1 + 12, n1 + 13, n1 + 14, n1 + 15, n1 + 16, n1 + 16, n1 + 17, n1 + 18, n1 + 19, n1 + 20, n1 + 21);
+    scanf("%d", &num1);
+    printf(" %d \n %d %d \n %d %d %d \n %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d %d %d \n ", num1, num1 + 1, num1 + 2, num1 + 3, num1 + 4, num1 + 5, num1 +6, num1 + 7, num1 + 8, num1 + 9 , num1 + 10, num1 + 11, num1 + 12, num1 + 13, num1 + 14, num1 + 15, num1 + 16, num1 + 16, num1 + 17, num1 + 18, num1 + 19, num1 + 20, num1 + 21);
 
 }
 
@@ -19,7 +19,7 @@ int main(){
 
 int main(){
 
-    int le =0, maior = 0, quant = 0;
+    int let = 0, maior = 0, quant = 0;
     for (int i = 1; i <= 7; i++){
         printf("Digite um numero: ");
         scanf("%d", &le);
@@ -49,9 +49,9 @@ int main(){
     if(km >= 75){
         printf("%d KM Acima da velocidade: MULTA E MENOS 5 PONTOS NA CARTEIRA \n", km);
     } else if (km < 75 && km >= 65){
-        printf("%d KM Voce ta um pouco veloz diminua a velocidade \n", km);
+        printf("%d KM Voce ta correndo um pouco, diminua a velocidade \n", km);
     } else if (km < 65 && km >= 55){
-        printf("%d KM Velocidade Normal continue assim \n", km);
+        printf("%d KM Velocidade Normal, continue assim \n", km);
     } else if (km < 55 && km >= 45){
         printf("%d KM Velocidade um pouco baixa aumente porfavor \n", km);
     } else{
